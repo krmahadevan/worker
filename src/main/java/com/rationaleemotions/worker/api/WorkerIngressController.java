fbe7d30d83ca076e5eb2ff0ab26f633cfda9daaa
Krishnan Mahadevan <krishnan.mahadevan1978@gmail.com>
2024-04-19 10:12:40 +0530

    Make headers aware

--- src/main/java/com/rationaleemotions/worker/api/WorkerIngressController.java
+++ src/main/java/com/rationaleemotions/worker/api/WorkerIngressController.java
@@ -1,20 +1,30 @@
 package com.rationaleemotions.worker.api;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/worker")
+@Slf4j
 public class WorkerIngressController {
 
+    @CrossOrigin("*")
     @GetMapping(path = "/greet", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Greeting> greeting() {
-        return ResponseEntity.ok(new Greeting(UUID.randomUUID().toString()));
+    public ResponseEntity<Greeting> greeting(@RequestHeader Map<String, String> headers) {
+        HttpHeaders outgoingHeaders = new HttpHeaders();
+        outgoingHeaders.set("X-Worker-Id", UUID.randomUUID().toString());
+        headers.forEach(outgoingHeaders::set);
+        ResponseEntity<Greeting> reply = ResponseEntity.ok()
+                .headers(outgoingHeaders)
+                .body(new Greeting("Greetings from worker " + UUID.randomUUID()));
+        log.info("Reply: {}", reply);
+        return reply;
     }
 
     public record Greeting(String message) {
